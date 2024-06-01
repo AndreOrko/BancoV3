5bb2fe219abb3601c35116a38a673f62dae59ce3
André Orko <sargentoandrericardo@hotmail.com>
2024-06-01 08:35:32 -0300

    versao1

--- bancoV3.py
+++ bancoV3.py
@@ -1,2 +1,26 @@
-print('hello world')
-print('Esta é a terceira Versão')
+class Conta:
+    def __init__(self, saldo, numero, agencia, cliente, historico):
+        self._saldo = saldo
+        self._numero = numero
+        self._agencia = agencia
+        self._cliente = cliente
+        self._historico = historico
+    
+
+    def saldo(self):
+        pass
+    
+
+    def nova_conta(self, cliente, numero):
+        pass
+
+
+    def sacar(self, valor):
+        pass
+
+
+    def depositar(self, valor):
+        pass
+
+
+class ContaCorrente(Conta):
